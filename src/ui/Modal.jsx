05bbbcdf488a98b49612a745f3163cffed9cf34d
Suhailkhan12 <suhail9717k@gmail.com>
2024-03-05 11:31:43 +0530

    modal correction

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -3,14 +3,22 @@ import { cloneElement, useContext } from "react";
 import { createContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
-import useOutsideClick from "../hooks/useOutsideClick";
+// import useOutsideClick from "../hooks/useOutsideClick";
 
 const ModalContext = createContext();
 
 function Modal({ children }) {
   const [openName, setOpenName] = useState("");
 
-  const close = () => setOpenName("");
+  // const close = (d) => {
+  //   d.preventDefault();
+  //   setOpenName("");
+  // };
+
+  function close(e) {
+    e.preventDefault();
+    setOpenName("");
+  }
   const open = setOpenName;
 
   return (
@@ -29,13 +37,14 @@ function Open({ children, opens: opensWindowName }) {
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
 
-  const { ref } = useOutsideClick(close);
+  // const { ref } = useOutsideClick(close);
 
   if (name !== openName) return null;
 
   return createPortal(
     <div className="overlay">
-      <div className="styledmodal" ref={ref}>
+      {/* <div className="styledmodal" ref={ref}> */}
+      <div className="styledmodal">
         <button className="buttonmodel" onClick={close}>
           <HiXMark />
         </button>
